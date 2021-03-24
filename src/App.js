789180a694c412ea505be1e658a52818339f30f7
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-24 19:42:04 -0400

    Changed CSS and added general wireframe

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MangaMeat</h1>
       </header>
+      <div className="feed">
+        {/* <h5>FEED</h5> */}
+        <div className="post">
+          
+          <div className="author">
+            <h3>poster's username</h3>
+            {/* poster's profile picture, replace <p> with image? */}
+            <p className="profilePicture">A</p>
+            <p>Post text</p>
+            {/* poster's submitted picture replace <p> with image?*/}
+            <p className="postPicture">B</p>
+            <button className="submit-comment">COMMENT</button>
+          </div>
+
+          <div className="comments">
+            <h3>username</h3>
+            <p>comment</p>
+            <button className="edit">EDIT</button>
+            <button className="delete">DELETE</button>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
