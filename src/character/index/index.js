727c4120a479f92c00bad31e9711281c99bce45f
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-26 16:35:24 -0400

    handle create in app.js

--- src/character/index/index.js
+++ src/character/index/index.js
@@ -5,6 +5,6 @@ export default function Index(props) {
     return (<div>
         <h1>Characters</h1>
         {props.currentCharacter && props.currentCharacter.map(char => (
-        <div onClick={()=>props.setDetail(char._id)}key={char._id} >{char.Name}</div>))}
+        <div onClick={()=>props.setDetail(char._id)}key={char._id}>{char.Name}</div>))}
         </div>)
 }
