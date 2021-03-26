1eaf35802f282bf17b87ab6211b749327125c9c0
Jigmey Dorjee <jdorjee@Jigmeys-MacBook-Air.local>
2021-03-26 13:27:27 -0400

    CRUD character edit

---
+++ src/character/edit/edit.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default class Detail extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: { ...this.props.data }
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        this.props.updateBookmarks(this.state.data)
+
+        //call local fetch
+        //do something that causes rerender of app (setting state via props)
+    }
+    //fetch call 
+
+    handleFormChange = e => this.setState({data: { ...this.state.data, [e.target.Name]: e.target.value }})
+    
+    render() {
+        return (
+            <form onSubmit={this.handleFormSubmit}>
+                <label htmlFor="name">Enter New Character Name</label>
+                <input id="name" name="name" type="text" placeholder="enter character name" value={this.state.data.Name} required onChange={this.handleFormChange} />
+                <br />
+                <label htmlFor="url">Enter New URL Name</label>
+                <input id="url" name="url" type="text" placeholder="enter school name" value={this.state.data.School} required onChange={this.handleFormChange} />
+                <br/>
+                <button type="submit">Submit Edits</button>
+            </form>)
+    }
+}
