167e6f49b7db98833ded6cbf2d480e032a4921d9
Erika Sanchez <erika.walker.business@gmail.com>
2021-03-28 19:10:29 -0400

    Deleted content to get rid of feed on App.js.

--- src/App.js
+++ src/App.js
@@ -191,30 +191,6 @@ class App extends Component {
         <header className="App-header">
           <h1>MangaMeat</h1>
         </header>
-        <div className="feed">
-          {/* <h5>FEED</h5> */}
-          <div className="post">
-            <div className="author">
-              <h3>character name</h3>
-              {/* poster's profile picture, replace <p> with image? */}
-              <p className="profilePicture">A</p>
-              <p>Post text</p>
-              {/* poster's submitted picture replace <p> with image?*/}
-              <p className="postPicture">B</p>
-              <button className="submit-comment">CREATE COMMENT</button>
-            </div>
-
-            <div className="comments">
-              <h3>username</h3>
-              <p>comment</p>
-              <button className="edit">EDIT</button>
-              <button className="delete">DELETE</button>
-            </div>
-
-          </div>
-
-        </div>
-
       </div>
     )
   }
